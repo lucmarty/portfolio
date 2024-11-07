2ff3a43a2952138af825ecd8fa9a3dcf87dff557
Marty Lucas <lucas.marty@etu.u-bordeaux.fr>
2024-11-07 16:34:23 +0100

    modify nextjs.yml and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true,
+    distDir: "dist",
     images: {
         unoptimized: true,
     },
