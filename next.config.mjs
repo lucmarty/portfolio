d28c09bc20295efc1cc4b2da926a62b640342eac
Marty Lucas <lucas.marty@etu.u-bordeaux.fr>
2024-11-07 16:57:07 +0100

    ty to deploy with gh

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: "export",
-    distDir: "dist",
+    reactStrictMode: true,
     images: {
         unoptimized: true,
     },
+    assetPrefix: isProd ? '/portfolio/' : '',
+    basePath: isProd ? '/portfolio' : '',
 };
 
 export default nextConfig;
