45467a83047e8f96cc2c508437dc6d47e09063f5
lucmarty <lucasmarty133@gmail.com>
2025-10-21 19:08:59 +0200

    titre description

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -7,14 +7,10 @@ export const Hero = () => {
     <Section className="flex max-md:flex-col items-start">
       <div className="flex-[2] w-full flex flex-col gap-2">
         <BlurFade delay={0.1} inView><h2 className="font-satoshi_bold text-9xl text-primary leading-tight ml-0">Lucas Marty</h2></BlurFade>
-        <BlurFade delay={0.1*2} inView><h3 className="font-satoshi text-5xl leading-tight ml-1.5">Etudiant en Informatique</h3></BlurFade>
+        <BlurFade delay={0.1*2} inView><h3 className="font-satoshi text-5xl leading-tight ml-1.5">Apprenti Data Science & IA </h3></BlurFade>
         <BlurFade delay={0.1*3} inView>
           <p className="font-roboto ml-2">
-            Après mon BUT Informatique, je souhaite poursuivre mes études en intégrant une école spécialisée dans le 
-            développement en Intelligence Artificielle. Passionné par la programmation, je développe des logiciels et des applications web dans 
-            divers langages tels que Java, Python, et C. <br/>
-            En parallèle, j&apos;explore les technologies modernes comme React et le framework Next.js 
-            pour approfondir mes compétences en développement web.
+           
           </p>
         </BlurFade>
     </div>
